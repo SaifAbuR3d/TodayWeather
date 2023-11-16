5f421bd5b5d1157851803e3e9123f58ed0fd787e
SaifAbuR3d <saefraed2@gmail.com>
2023-11-16 07:11:50 +0200

    Implement logic to validate users credentials

---
+++ AuthenticationService.cs
@@ -0,0 +1,14 @@
+﻿using TodayWeather.Models;
+
+namespace TodayWeather.Services;
+
+public static class AuthenticationService
+{
+    public static User? ValidateUserCredentials(string? userName, string? password)
+    {
+        if (string.IsNullOrEmpty(userName) || string.IsNullOrEmpty(password)) return null;
+
+        var user = User.Users.FirstOrDefault(e => e.UserName == userName && e.Password == password);
+        return user;
+    }
+}

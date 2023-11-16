7c7e4ee081945093843dc3f597381931f235686e
SaifAbuR3d <saefraed2@gmail.com>
2023-11-16 07:53:16 +0200

    Apply Authorization rule The user's email address host must be 'gmail.com' to gain access to api/getweatherforecast endpoint.

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Mail;
 using TodayWeather.Models;
 
 namespace TodayWeather.Controllers;
@@ -19,14 +21,26 @@ public class WeatherForecastController : ControllerBase
         _logger = logger;
     }
 
+
+    [Authorize]
     [HttpGet(Name = "GetWeatherForecast")]
-    public WeatherForecast Get()
+    public ActionResult<WeatherForecast> Get()
     {
-        return new WeatherForecast
+        // get email address from token's claims
+        var email_address = new MailAddress(User.Claims.FirstOrDefault(e => e.Type == "email_address")?.Value.Trim()); 
+
+        // only accept 'gmail.com' host name
+        string host = email_address.Host.Trim();
+        if (host != "gmail.com")
+        {
+            return Forbid();
+        }
+
+        return Ok(new WeatherForecast
         {
             Date = DateOnly.FromDateTime(DateTime.Now),
             TemperatureC = Random.Shared.Next(-20, 55),
             Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-        };
+        });
     }
 }
